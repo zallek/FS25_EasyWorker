b6165d16fc1675f671ae429f77f92cbead5e61ef
Nicolas Fortin <nicolas.fortin37@gmail.com>
2025-03-13 12:21:14 +0100

    Add shortcut to open worker dialog

---
+++ scripts/vehicules/specializations/AIModeSelection.lua
@@ -0,0 +1,32 @@
+EW_AIModeSelection = {}
+
+function EW_AIModeSelection.onRegisterActionEvents(self, _, _)
+	if self.isClient then
+		local spec = self.spec_aiModeSelection
+		if self:getIsActiveForInput(true, true) then
+			local _, actionEventId = self:addActionEvent(spec.actionEvents, InputAction.EASYWORKER_OPEN_AI_SCREEN, self, EW_AIModeSelection.onOpenAIScreen, false, true, false, true, nil)
+			AIModeSelection.updateActionEvents(self)
+		end
+	end
+end
+
+AIModeSelection.onRegisterActionEvents = Utils.appendedFunction(AIModeSelection.onRegisterActionEvents, EW_AIModeSelection.onRegisterActionEvents)
+
+function EW_AIModeSelection.updateActionEvents(self)
+	local spec = self.spec_aiModeSelection
+	local actionEvent = spec.actionEvents[InputAction.EASYWORKER_OPEN_AI_SCREEN]
+	if actionEvent ~= nil and self.isActiveForInputIgnoreSelectionIgnoreAI then
+		g_inputBinding:setActionEventText(actionEvent.actionEventId, g_i18n:getText("input_EASYWORKER_OPEN_AI_SCREEN"))
+		g_inputBinding:setActionEventTextPriority(actionEvent.actionEventId, GS_PRIO_HIGH)
+		g_inputBinding:setActionEventActive(actionEvent.actionEventId, self:getCanToggleAIVehicle())
+	end
+end
+
+AIModeSelection.updateActionEvents = Utils.appendedFunction(AIModeSelection.updateActionEvents, EW_AIModeSelection.updateActionEvents)
+
+function EW_AIModeSelection.onOpenAIScreen()
+    local vehicle = g_localPlayer.getCurrentVehicle()
+    g_inGameMenu:openAIScreen(vehicle)
+end
+
+-- startGoToJob
